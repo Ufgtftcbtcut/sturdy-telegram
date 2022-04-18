3e9219d15914d6b60976656d5a096c17a1f7f777
Ufgtftcbtcut <103160162+Ufgtftcbtcut@users.noreply.github.com>
2022-04-18 14:53:30 +0700

    Create jstnnnnnn.cpp

---
+++ jstnnnnnn.cpp
@@ -0,0 +1,63 @@
+// Imports
+#include <iostream>
+#include <vector>
+#include <tuple>
+#include <string>
+
+// Set
+using namespace std;
+
+// Define code
+const string codeToRun = R"("
+	                    
+	                    
+	                    
+het
+	                   
+	                   
+	                  
+)";
+    
+// Constants
+const string nl = "\n";
+const string sl = "-------------------------";
+const string usl = "_________________";
+
+// Variables
+vector<string> vram;
+
+// Configs
+/*
+unordered_map<string, bool> boolConfigs = {};
+unordered_map<string, int> intConfigs;
+unordered_map<string, string> strConfigs;
+unordered_map<string, string> interfaceElements;
+
+unordered_map boolConfigs.emplace("hrya",true);
+
+it would always think the maps are types :(
+*/
+
+// General functions
+void printStr(string text = "") {
+	cout << text.append(nl);
+}
+
+// Run multiple lines
+void runPiznCode(string codeInitGot) {
+}
+
+// Storify
+auto typeStorify(string type,string content) {
+	return tuple<string> {"stochi"};
+}
+
+// int main
+int main() {
+	printStr("/\n| %™©® Pizn Interpreter ®©™%\n\\\n\n...\n\n");
+    runPiznCode(codeToRun);
+    
+    printStr(get<0>(typeStorify("","")));
+    
+	return 0;
+}
